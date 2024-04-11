af4d6297bf188a1106fce0945b6de537aaa994d4
Koen Luyten <kluyten@xpirit.com>
2024-04-11 15:58:25 +0200

    Lab 2 - Adding more methods

--- PieShopApi/Controllers/PiesController.cs
+++ PieShopApi/Controllers/PiesController.cs
@@ -5,19 +5,49 @@ namespace PieShopApi.Controllers
 {
     [ApiController]
     [Route("pies")]
-    public class PiesController: ControllerBase
+    public class PiesController : ControllerBase
     {
         [HttpGet]
         public JsonResult GetPies()
         {
-            var pies = new List<Pie>
+            return new JsonResult(PieStore.GetAll());
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ActionResult<Pie> GetPie(int id)
+        {
+            return PieStore.GetById(id);
+        }
+
+        [HttpGet]
+        [Route("search")]
+        public ActionResult<Pie> SearchPie(string name)
+        {
+            return PieStore.GetByPartialName(name);
+        }
+    }
+
+    public class PieStore
+    {
+        public static IEnumerable<Pie> GetAll()
+        {
+            return new List<Pie>
             {
-                new Pie { Id = 1, Name = "Apple Pie", Description = "Our famous apple pies!" },
-                new Pie { Id = 2, Name = "Blueberry Pie", Description = "You'll love our blueberry pies!" },
-                new Pie { Id = 3, Name = "Cherry Pie", Description = "Our cherry pies are the best!" }
+                new Pie { Id = 1, Name = "Apple Pie", Description = "Tasty" },
+                new Pie { Id = 2, Name = "Cherry Pie", Description = "Yummy" },
+                new Pie { Id = 3, Name = "Pumpkin Pie", Description = "Delicious" }
             };
-            
-            return new JsonResult(pies);
+        }
+
+        public static Pie GetById(int id)
+        {
+            return GetAll().SingleOrDefault(p => p.Id == id);
+        }
+
+        public static Pie GetByPartialName(string name)
+        {
+            return GetAll().FirstOrDefault(p => p.Name.ToLowerInvariant().Contains(name.ToLowerInvariant()));
         }
     }
 }
